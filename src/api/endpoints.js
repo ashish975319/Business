64f143a0637ac52c862be2850acbb9e9f420ba1e
ashish975319 <ashish975319@users.noreply.github.com>
2024-08-22 05:28:28 +0530

    Base URL Change.

--- src/api/endpoints.js
+++ src/api/endpoints.js
@@ -1,4 +1,7 @@
-export const API_BASE_URL = "http://10.0.0.140:5959/Qr-Code/api";
+// export const API_BASE_URL = "http://10.0.0.140:5959/Qr-Code/api";
+
+export const API_BASE_URL =
+  "https://ashish-business-qrcode-production.up.railway.app/api";
 
 export const ENDPOINTS = {
   CREATE_USERS: `${API_BASE_URL}/createUsers`,
