c55f7f77cbd4c12caa6488e23241e74b47e073b4
ashish975319 <ashish975319@users.noreply.github.com>
2024-08-21 18:55:45 +0530

    tomcat url

--- src/api/endpoints.js
+++ src/api/endpoints.js
@@ -1,4 +1,4 @@
-export const API_BASE_URL = "http://localhost:8888/api";
+export const API_BASE_URL = "http://10.0.0.140:5959/Qr-Code/api";
 
 export const ENDPOINTS = {
   CREATE_USERS: `${API_BASE_URL}/createUsers`,
